8f9ce469af7d76d4c181dc5c0580adfb16db2178
zzaizzai <junsai0530@gmail.com>
2024-12-09 23:46:39 +0900

    graph setting

--- src/chartbase.js
+++ src/chartbase.js
@@ -15,52 +15,141 @@ var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (
 }) : function(o, v) {
     o["default"] = v;
 });
-var __importStar = (this && this.__importStar) || (function () {
-    var ownKeys = function(o) {
-        ownKeys = Object.getOwnPropertyNames || function (o) {
-            var ar = [];
-            for (var k in o) if (Object.prototype.hasOwnProperty.call(o, k)) ar[ar.length] = k;
-            return ar;
-        };
-        return ownKeys(o);
-    };
-    return function (mod) {
-        if (mod && mod.__esModule) return mod;
-        var result = {};
-        if (mod != null) for (var k = ownKeys(mod), i = 0; i < k.length; i++) if (k[i] !== "default") __createBinding(result, mod, k[i]);
-        __setModuleDefault(result, mod);
-        return result;
-    };
-})();
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.createGraph = createGraph;
+exports.createGraph = void 0;
 var echarts = __importStar(require("echarts"));
-function createGraph(id) {
-    var chartDom = document.getElementById(id);
+function createGraph(idString, forecastData, resultData) {
+    var chartDom = document.getElementById(idString);
     var myChart = echarts.init(chartDom);
+    var COLORS = [
+        '#FF6F61', // Red (Coral Red)
+        '#5B84B1', // Blue (Steel Blue)
+        '#88B04B', // Green (Olive Green)
+        '#FFD700', // Yellow (Golden Yellow)
+        '#9B59B6', // Purple (Amethyst Purple)
+        '#00A5CF', // Cyan (Deep Cyan)
+        '#FFA500', // Orange (Bright Orange)
+        '#FF00FF', // Magenta (Vivid Magenta)
+        '#32CD32', // Lime (Lime Green)
+        '#FFC0CB' // Pink (Light Pink)
+    ];
+    var xAxisData = [];
+    var dataForecast1 = [];
+    var dataForecast2 = [];
+    var dataForecast3 = [];
+    var dataResult1 = [];
+    var dataResult2 = [];
+    var dataResult3 = [];
+    for (var i = 0; i < 10; i++) {
+        xAxisData.push('Group' + i);
+        dataForecast1.push(+(Math.random() * 10).toFixed(2));
+        dataForecast2.push(+(Math.random() * 5).toFixed(2));
+        dataForecast3.push(+(Math.random() * 3).toFixed(2));
+        dataResult1.push(+(Math.random() + 10).toFixed(2));
+        dataResult2.push(+(Math.random() * 5).toFixed(2));
+        dataResult3.push(+(Math.random() * 3).toFixed(2));
+    }
+    var emphasisStyle = {
+        itemStyle: {
+            shadowBlur: 10,
+            shadowColor: 'rgba(0,0,0,0.3)'
+        }
+    };
     var option = {
+        legend: {
+            data: ['forecast1', 'forecast2', 'forecast3', 'result1', 'result2', 'result3'],
+            align: 'left'
+        },
+        toolbox: {
+            feature: {
+                magicType: {
+                // type: ['stack', 'tiled']
+                },
+                dataView: {},
+                saveAsImage: {
+                    pixelRatio: 2
+                }
+            }
+        },
         title: {
-            text: 'Simple Graph'
+            text: 'Graph Demo'
         },
         tooltip: {},
         xAxis: {
-            data: ['A', 'B', 'C', 'D', 'E', 'F']
+            data: ['Mon', 'Tus', 'Web', 'Tur', 'Fri', 'Sat', 'Sun'],
+            name: 'Day',
+            axisLine: { onZero: true },
+            splitLine: { show: false },
+            splitArea: { show: false }
         },
         yAxis: {},
+        textStyle: {
+            fontSize: 20,
+            color: '#333'
+        },
         series: [
             {
-                name: 'Score',
+                name: 'forecast1',
                 type: 'bar',
-                data: [5, 20, 36, 10, 10, 20]
+                stack: 'one',
+                emphasis: emphasisStyle,
+                data: dataForecast1,
+                color: COLORS[0]
+            },
+            {
+                name: 'forecast2',
+                type: 'bar',
+                stack: 'one',
+                emphasis: emphasisStyle,
+                data: dataForecast2,
+                color: COLORS[1]
+            },
+            {
+                name: 'forecast3',
+                type: 'bar',
+                stack: 'one',
+                emphasis: emphasisStyle,
+                data: dataForecast3,
+                color: COLORS[2]
+            },
+            {
+                name: 'result1',
+                type: 'bar',
+                stack: 'two',
+                emphasis: emphasisStyle,
+                data: dataResult1,
+                color: COLORS[0]
+            },
+            {
+                name: 'result2',
+                type: 'bar',
+                stack: 'two',
+                emphasis: emphasisStyle,
+                data: dataResult2,
+                color: COLORS[1]
+            },
+            {
+                name: 'result3',
+                type: 'bar',
+                stack: 'two',
+                emphasis: emphasisStyle,
+                data: dataResult3,
+                color: COLORS[2]
             }
-        ],
-        grid: {
-            left: '100%',
-            right: '100%',
-            bottom: '100%',
-            top: '200%',
-            containLabel: true
-        }
+        ]
     };
     myChart.setOption(option);
+    myChart.on('click', function (params) { console.log("good"); }); // click event
+    myChart.resize({
+        width: 1000,
+        height: 800
+    });
 }
+exports.createGraph = createGraph;
--- src/chartbase.ts
+++ src/chartbase.ts
@@ -1,38 +1,146 @@
 import * as echarts from 'echarts';
 
 
-export function createGraph(idString: string) {
-
+export function createGraph(idString: string, forecastData: any[], resultData: any[]): void {
 
     const chartDom = document.getElementById(idString) as HTMLElement;
     const myChart = echarts.init(chartDom);
 
-    const option = {
-        title: {
-            text: 'Simple Graph'
-        },
-        tooltip: {},
-        xAxis: {
-            data: ['A', 'B', 'C', 'D', 'E', 'F']
-        },
-        yAxis: {},
-        series: [
-            {
-                name: 'Score',
-                type: 'bar',
-                data: [5, 20, 36, 10, 10, 20]
-            }
-        ],
-        grid: {
-            left: '100%',
-            right: '100%',
-            bottom: '100%',
-            top: '200%',
-            containLabel: true
+    const COLORS: string[] = [
+        '#FF6F61', // Red (Coral Red)
+        '#5B84B1', // Blue (Steel Blue)
+        '#88B04B', // Green (Olive Green)
+        '#FFD700', // Yellow (Golden Yellow)
+        '#9B59B6', // Purple (Amethyst Purple)
+        '#00A5CF', // Cyan (Deep Cyan)
+        '#FFA500', // Orange (Bright Orange)
+        '#FF00FF', // Magenta (Vivid Magenta)
+        '#32CD32', // Lime (Lime Green)
+        '#FFC0CB'  // Pink (Light Pink)
+    ];
+
+
+    let xAxisData: string[] = [];
+    let dataForecast1: number[] = [];
+    let dataForecast2: number[] = [];
+    let dataForecast3: number[] = [];
+
+    let dataResult1: number[] = [];
+    let dataResult2: number[] = [];
+    let dataResult3: number[] = [];
+
+    for (let i = 0; i < 10; i++) {
+
+        xAxisData.push('Group' + i);
+
+        dataForecast1.push(+(Math.random() * 10).toFixed(2));
+        dataForecast2.push(+(Math.random() * 5).toFixed(2));
+        dataForecast3.push(+(Math.random() * 3).toFixed(2));
+
+        dataResult1.push(+(Math.random() + 10).toFixed(2));
+        dataResult2.push(+(Math.random() * 5).toFixed(2));
+        dataResult3.push(+(Math.random() * 3).toFixed(2));
+
+    }
+
+    var emphasisStyle = {
+        itemStyle: {
+            shadowBlur: 10,
+            shadowColor: 'rgba(0,0,0,0.3)'
         }
     };
 
-    myChart.setOption(option);
 
+    const option: echarts.EChartsCoreOption = {
+        legend: {
+            data: ['forecast1', 'forecast2', 'forecast3', 'result1', 'result2', 'result3'],
+            align: 'left'
+        },
+        toolbox: {
+            feature: {
+                magicType: {
+                    // type: ['stack', 'tiled']
+                },
+                dataView: {},
+                saveAsImage: {
+                    pixelRatio: 2
+                }
+            }
+        },
+        title: {
+            text: 'Graph Demo'
+        },
+        tooltip: {},
+        xAxis: {
+            data: ['Mon', 'Tus', 'Web', 'Tur', 'Fri', 'Sat', 'Sun'],
+            name: 'Day',
+            axisLine: { onZero: true },
+            splitLine: { show: false },
+            splitArea: { show: false }
+        },
+        yAxis: {},
+        textStyle: {
+            fontSize: 20,
+            color: '#333'
+        },
+        series: [
+            {
+                name: 'forecast1',
+                type: 'bar',
+                stack: 'one',
+                emphasis: emphasisStyle,
+                data: dataForecast1,
+                color: COLORS[0]
+            },
+            {
+                name: 'forecast2',
+                type: 'bar',
+                stack: 'one',
+                emphasis: emphasisStyle,
+                data: dataForecast2,
+                color: COLORS[1]
+            },
+            {
+                name: 'forecast3',
+                type: 'bar',
+                stack: 'one',
+                emphasis: emphasisStyle,
+                data: dataForecast3,
+                color: COLORS[2]
+            },
+            {
+                name: 'result1',
+                type: 'bar',
+                stack: 'two',
+                emphasis: emphasisStyle,
+                data: dataResult1,
+                color: COLORS[0]
+            },
+            {
+                name: 'result2',
+                type: 'bar',
+                stack: 'two',
+                emphasis: emphasisStyle,
+                data: dataResult2,
+                color: COLORS[1]
+            },
+            {
+                name: 'result3',
+                type: 'bar',
+                stack: 'two',
+                emphasis: emphasisStyle,
+                data: dataResult3,
+                color: COLORS[2]
+            }
+        ]
+    };
+
+    myChart.setOption<echarts.EChartsCoreOption>(option);
+    myChart.on('click', function (params) { console.log("good") }); // click event
+
+    myChart.resize({
+        width: 1000,
+        height: 800
+    })
 }
 
